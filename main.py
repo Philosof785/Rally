72691d9d6f639a5fa4486912886e1e9938784715
Philosof785 <kbaranov833@gmail.com>
2020-11-02 10:22:25 +1000

    0

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import pygame as pg
 
 SIZE = WIDTH, HEIGHT = 800, 600
 GREY = (128, 128, 128)
+GREEN = (0, 128, 0)
+WHITE = (200, 200, 200)
 
 pg.init()
 pg.display.set_caption("Rally")
 screen = pg.display.set_mode(SIZE)
+
+FPS = 120
+clock = pg.time.Clock()
+
+bg_image = pg.image.load("Image/road.jpg")
+bg_image_rect = bg_image.get_rect(topleft=(0, 0))
+bg_image_2_rect = bg_image.get_rect(topleft=(0, -HEIGHT))
+
+
+
+def bg():
+    pg.draw.line(screen, GREEN, (20, 0), (20, 600), 40)
+    pg.draw.line(screen, GREEN, (780, 0), (780, 600), 40)
+    for xx in range(10):
+        for yy in range(10):
+            pg.draw.line(screen, WHITE, 
+            (40 + xx * 80, 0 if xx == 0 or xx == 9 else 10 + yy * 60),
+            (40 + xx * 80, 600 if xx == 0 or xx == 9 else 50 + yy * 60), 5)
 
 
 class Car(pg.sprite.Sprite):
@@ -30,9 +50,21 @@ while game:
     if car1.y < -car1_h:
        car1.y = HEIGHT
 
+    bg_image_rect.y += 1
+    if bg_image_rect.y > HEIGHT:
+        bg_image_rect.y = 0
+    bg_image_2_rect.y += 1
+    if bg_image_2_rect.y > 0:
+        bg_image_2_rect.y = -HEIGHT
+
 
     screen.fill(GREY)
+    # bg()
+    screen.blit(bg_image, bg_image_rect)
+    screen.blit(bg_image, bg_image_2_rect)
     screen.blit(car1_image, (car1.x, car1.y))
     pg.display.update() 
-    pg.time.wait(5)       
- 
+    clock.tick(FPS)
+    pg.display.set_caption(f"Rally     FPS: {int(clock.get_fps())}")
+
+pg.image.save(screen, "road.jpg")
